6f1d08302aad168e642834fdc2c24a468b013af9
dipaloke <dipalokebiswas96@gmail.com>
2023-11-22 13:04:33 +0600

    Finished encryption section

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -3,6 +3,7 @@ import {
   Frontend_skill,
   Full_stack,
   Other_skill,
+  Skill_data,
 } from "@/constants";
 import React from "react";
 import SkillDataProvider from "../sub/SkillDataProvider";
@@ -13,9 +14,20 @@ const Skills = () => {
     <section
       id="skills"
       style={{ transform: "scale(0.9)" }}
-      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20"
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
     >
       <SkillText />
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {Skill_data.map((image, index) => (
+          <SkillDataProvider
+            key={index}
+            src={image.Image}
+            width={image.width}
+            height={image.height}
+            index={index}
+          />
+        ))}
+      </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Frontend_skill.map((image, index) => (
           <SkillDataProvider
